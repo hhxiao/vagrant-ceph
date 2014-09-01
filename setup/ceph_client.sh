7239d8c315824d9c54ee8dddb3bfeed894005be6
hhxiao <hhxiao@gmail.com>
2014-09-01 14:23:43 -0700

    shell script to provision a ceph fuse client

--- setup/ceph_client.sh
+++ setup/ceph_client.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-sudo wget -q -O- 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc' | sudo apt-key add - 
-sudo echo deb http://ceph.com/debian-firefly/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list 
-sudo apt-get update && sudo apt-get install -y ceph-deploy
+#sudo ceph-deploy new ceph-client
+#sudo ceph-deploy install ceph-client
 
-sudo sed -i '$ a\192.168.251.100 ceph' /etc/hosts
-sudo sed -i '$ a\192.168.251.101 ceph-client' /etc/hosts
-sudo ceph-deploy new ceph-client
-sudo ceph-deploy install ceph-client
+#sudo cat /vagrant/setup/keyring | grep -e "key" | cut  -c8- | awk '{ print "sudo mount -t ceph ceph:6789:/ /mnt/cephfs -o name=admin,secret="$1}' > /tmp/mount.sh
+#sudo sh /tmp/mount.sh
 
 sudo mkdir /mnt/cephfs
-sudo cat /vagrant/setup/keyring | grep -e "key" | cut  -c8- | awk '{ print "sudo mount -t ceph ceph:6789:/ /mnt/cephfs -o name=admin,secret="$1}' > /tmp/mount.sh
-sudo sh /tmp/mount.sh
+sudo mkdir /etc/ceph && mv /vagrant/setup/ceph.client.admin.keyring /etc/ceph/
+sudo apt-get install -y ceph-fuse
+sudo ceph-fuse -m ceph:6789 /mnt/cephfs
+sudo chown -R vagrant:vagrant /mnt
