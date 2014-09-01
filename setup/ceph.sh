233679d75b886a5c0683d54ca8bfaa06c894d53c
hhxiao <hhxiao@gmail.com>
2014-09-01 14:23:27 -0700

    shell script to provision ceph server

--- setup/ceph.sh
+++ setup/ceph.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+HOST=`hostname -s`
+DRV="bcdefghijklmnopqrstuvwxyz"
+
+if [ -n "$1" ]; then
+	NUM=$1
+else
+	NUM=3
+fi
+
 sudo wget -q -O- 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc' | sudo apt-key add - 
 sudo echo deb http://ceph.com/debian-firefly/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list 
 sudo apt-get update && sudo apt-get install -y ceph-deploy
 
-sudo sed -i '$ a\192.168.251.100 ceph' /etc/hosts
-sudo sed -i '$ a\192.168.251.101 ceph-client' /etc/hosts
-sudo ceph-deploy new ceph
-sudo ceph-deploy install ceph
-sudo echo "osd crush chooseleaf type = 0" >> ceph.conf
-sudo ceph-deploy mon create ceph
-sudo ceph-deploy gatherkeys ceph
-sudo ceph-deploy osd create ceph:/dev/sdb ceph:/dev/sdc ceph:/dev/sdd
-sudo ceph-deploy mds create ceph
-sudo ceph-deploy admin ceph
-sudo rm -f /vagrant/setup/keyring && sudo cp ./ceph.client.admin.keyring /vagrant/setup/keyring
+sudo ceph-deploy new ${HOST}
+sudo ceph-deploy install ${HOST}
+sudo sed -i '$ a\osd crush chooseleaf type = 0' ceph.conf
+sudo ceph-deploy mon create ${HOST}
+sudo ceph-deploy gatherkeys ${HOST}
+for (( i=0; i<${#DRV}&&i<NUM; i++ )); do
+  sudo ceph-deploy osd create --zap-disk ${HOST}:/dev/sd${DRV:$i:1}
+done
+sudo ceph-deploy mds create ${HOST}
+sudo ceph-deploy admin ${HOST}
+sudo rm -f /vagrant/setup/ceph.client.admin.keyring && sudo cp ./ceph.client.admin.keyring /vagrant/setup/ceph.client.admin.keyring
