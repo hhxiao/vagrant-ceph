a176d593b76507e891118a4bd4944657e5a30fa2
Haihua Xiao <hhxiao@gmail.com>
2014-09-12 08:49:23 +0800

    no message

--- setup/ceph_client.sh
+++ setup/ceph_client.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo wget -q -O- 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc' | sudo apt-key add - 
+sudo echo deb http://ceph.com/debian-firefly/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list 
+
 #sudo ceph-deploy new ceph-client
 #sudo ceph-deploy install ceph-client
 
@@ -7,7 +10,8 @@
 #sudo sh /tmp/mount.sh
 
 sudo mkdir /mnt/cephfs
-sudo mkdir /etc/ceph && mv /vagrant/setup/ceph.client.admin.keyring /etc/ceph/
-sudo apt-get install -y ceph-fuse
+sudo mkdir /etc/ceph && cp /vagrant/setup/ceph.client.admin.keyring /etc/ceph/
+sudo apt-get update && sudo apt-get install -y ceph-fuse
 sudo ceph-fuse -m ceph:6789 /mnt/cephfs
 sudo chown -R vagrant:vagrant /mnt
+
